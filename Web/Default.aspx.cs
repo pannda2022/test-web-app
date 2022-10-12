fa55d23fd010e8af2e957029a3e0afd4aad17714
pannda2022 <115164800+pannda2022@users.noreply.github.com>
2022-10-13 00:15:39 +0900

    Add files via upload

---
+++ Web/Default.aspx.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.ApplicationInsights.Channel;
+using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            var channel = new InMemoryChannel();
+
+            try
+            {
+                IServiceCollection services = new ServiceCollection();
+                services.Configure<TelemetryConfiguration>(config => config.TelemetryChannel = channel);
+                services.AddLogging(builder =>
+                {
+                    // Only Application Insights is registered as a logger provider
+                    builder.AddApplicationInsights(
+                        configureTelemetryConfiguration: (config) => config.ConnectionString = "InstrumentationKey=1371ae0b-063d-42f9-9a7f-bc76d33f76e1;IngestionEndpoint=https://eastasia-0.in.applicationinsights.azure.com/;LiveEndpoint=https://eastasia.livediagnostics.monitor.azure.com/",
+                        configureApplicationInsightsLoggerOptions: (options) => { }
+                    );
+                });
+
+                IServiceProvider serviceProvider = services.BuildServiceProvider();
+                ILogger<Default> logger = serviceProvider.GetRequiredService<ILogger<Default>>();
+
+                logger.LogInformation("Information Logger is working...");
+                logger.LogWarning("Warning Logger is working...");
+                logger.LogError("Error Logger is working...");
+                logger.LogDebug("Debug Logger is working..."); //入らなかった
+                logger.LogCritical("Critical Logger is working...");
+                logger.LogTrace("Trace Logger is working..."); //入らなかった
+            }
+            finally
+            {
+                // Explicitly call Flush() followed by Delay, as required in console apps.
+                // This ensures that even if the application terminates, telemetry is sent to the back end.
+                channel.Flush();
+
+                //await Task.Delay(TimeSpan.FromMilliseconds(1000));
+            }
+        }
+    }
+}
---
+++ Web/SaveFile.aspx.cs
@@ -0,0 +1,32 @@
+﻿using Azure.Storage.Blobs;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Web
+{
+    public partial class SaveFile : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string azureConnectionString = "DefaultEndpointsProtocol=https;AccountName=testblobstorage2022cxw2;AccountKey=AAIg667FOr0n8umYvQL+pCjo8uUe+6s+gBOfBunb4ywQUkd+7MM1c3vrBiCF2iFX/0w9krHEo845+ASt8IjO9A==;EndpointSuffix=core.windows.net";
+            string containerName = "testcontainer";
+            BlobContainerClient container = new BlobContainerClient(azureConnectionString, containerName);
+
+            // アップロードするファイルパス
+            string fileName = Path.GetFileName("abc.txt");
+            BlobClient blobClient = container.GetBlobClient(fileName);
+
+            // アップロード
+            using (MemoryStream ms = new MemoryStream(Encoding.UTF8.GetBytes("試験ファイル")))
+            {
+                blobClient.Upload(ms, true);
+            };
+        }
+    }
+}
